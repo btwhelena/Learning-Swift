d4b3d7fa9e0669aef4ec65614310d10c1cd45c66
Matheus Holanda Matos <matheus-holanda@hotmail.com>
2022-10-02 20:54:12 -0300

    Create tuples.swift

---
+++ tuples.swift
@@ -0,0 +1,8 @@
+// refer statusLoad from above
+let (sMessage, sCode) = statusLoad
+
+// prints the message “Forbidden”
+print(“The status message is \(sMessage).”)
+
+// prints the code “403”
+print(“The status code is \(sCode).”)
